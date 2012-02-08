4d103898ac5e2583c7ea3a49852fd7154d21543b
ikeagold alasecond <test1@li.ru>
2012-02-08 16:02:20 +0200

    new 1.0.15

---
+++ src/ikeagold/zimniy/giolight/Install.java
@@ -0,0 +1,12 @@
+package ikeagold.zimniy.giolight;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Install extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.install);
+	}
+}
