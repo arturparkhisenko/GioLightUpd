8d81622cfd5559ebecf1a903487c14ce85db050c
ikeagold alasecond <test1@li.ru>
2012-03-01 16:37:13 +0200

    0.116 all inc and start debug

--- src/ikeagold/zimniy/giolight/UpdateService.java
+++ src/ikeagold/zimniy/giolight/UpdateService.java
@@ -2,10 +2,7 @@ package ikeagold.zimniy.giolight;
 
 import ikeagold.zimniy.giolight.GioLightUpdActivity;
 import ikeagold.zimniy.giolight.R;
-import ikeagold.zimniy.giolight.Preferences;
-
 import android.app.AlarmManager;
-import android.content.BroadcastReceiver;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
@@ -16,41 +13,38 @@ import android.app.NotificationManager;
 import android.os.IBinder;
 import android.os.SystemClock;
 import android.util.Log;
-import android.widget.Toast;
-import android.content.res.Resources;
-import android.net.Uri;
-import android.os.*;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.preference.PreferenceManager;
 
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
-import java.text.MessageFormat;
-import java.util.Date;
-import java.util.LinkedList;
-
 
 public class UpdateService extends Service {
-	
-	
+
+	public String glvc;
+	public String glvn;
+	public String str1;
+	public String str2;
+	public String furlt;
+	public String furl;
 
 	public static void schedule(Context context) {
-
 		SharedPreferences prefs = PreferenceManager
 				.getDefaultSharedPreferences(context);
-		
-		boolean Sync;
-		Sync = prefs.getBoolean("synckey", false);
+		boolean Sync = prefs.getBoolean("synckey", false);
 		if (Sync == false) {
 			// Nothing
 		} else {
 			String sint = prefs.getString("updateInterval", "24");
 			int interval = Integer.parseInt(sint);
 			Intent intent = new Intent(context, UpdateService.class);
-			PendingIntent pending = PendingIntent.getService(context, 0, intent, 0);
+			PendingIntent pending = PendingIntent.getService(context, 0,
+					intent, 0);
 			AlarmManager alarm = (AlarmManager) context
 					.getSystemService(Context.ALARM_SERVICE);
 			alarm.cancel(pending);
@@ -60,7 +54,6 @@ public class UpdateService extends Service {
 						AlarmManager.INTERVAL_HOUR, pending);
 			}
 		}
-
 	}
 
 	@Override
@@ -76,20 +69,15 @@ public class UpdateService extends Service {
 
 	// MAIN DEV SECTION
 	private void handleCommand() {
-
 		new Thread() {
 			public void run() {
-				
 				SharedPreferences prefs = PreferenceManager
 						.getDefaultSharedPreferences(getBaseContext());
-				
 				boolean Sync;
 				Sync = prefs.getBoolean("synckey", false);
 				if (Sync == false)
 					return;
-				
-				//
-
+				// Prefs
 				long lastUpdate = prefs.getLong("lastUpdateCheck", 0);
 				String sint = prefs.getString("updateInterval", "24");
 				int interval = Integer.parseInt(sint);
@@ -98,14 +86,17 @@ public class UpdateService extends Service {
 				if (lastUpdate + (interval * 60 * 60) > System
 						.currentTimeMillis())
 					return;
-
-				// Do the update...
-				
+				// Checking update
 				try {
-					
-					
 					// AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-					
+
+					if (isInternetOn()) {
+						update();
+					} else {
+						// Maybe toast
+					}
+
+					// AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
 				} catch (Exception e) {
 					Log.e("WTF",
 							"Exception during handleCommand():\n"
@@ -120,6 +111,119 @@ public class UpdateService extends Service {
 	@Override
 	public IBinder onBind(Intent intent) {
 		return null;
+	}
+
+	public void update() {
+		SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(getBaseContext());
+		boolean Test = prefs.getBoolean("testkey", false);
+		if (Test == false) {
+			str1 = DownloadText("http://gio-light.googlecode.com/hg/version.txt");
+			glvn = str1;
+			NewRom();
+		} else {
+			str2 = DownloadText("http://gio-light.googlecode.com/hg/version.testing.txt");
+			glvn = str2;
+			NewRom();
+		}
+	}
+
+	private void NewRomNotification() {
+		String ns = Context.NOTIFICATION_SERVICE;
+		NotificationManager mNotificationManager = (NotificationManager) getSystemService(ns);
+		int icon = R.drawable.ic_launcher;
+		CharSequence contentTitle = "GioLightUpd";
+		long when = System.currentTimeMillis();
+		Notification notification = new Notification(icon, contentTitle, when);
+		Context context = getApplicationContext();
+		CharSequence contentText = "Есть обновление ROM";
+		// ON CLICK *.class
+		Intent notificationIntent = new Intent(this, GioLightUpdActivity.class);
+		PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+				notificationIntent, 0);
+		notification.setLatestEventInfo(context, contentTitle, contentText,
+				contentIntent);
+		final int HELLO_ID = 1;
+		mNotificationManager.notify(HELLO_ID, notification);
+	}
+
+	private void NewRom() {
+		if (glvc.trim().equalsIgnoreCase(glvn.trim())) {
+		} else {
+			if (Integer.parseInt(glvc.trim()) < Integer.parseInt(glvn.trim())) {
+				NewRomNotification();
+			} else {
+			}
+		}
+	}
+
+	private InputStream OpenHttpConnection(String urlString) throws IOException {
+		InputStream in = null;
+		int response = -1;
+		URL url = new URL(urlString);
+		URLConnection conn = url.openConnection();
+		if (!(conn instanceof HttpURLConnection))
+			throw new IOException("Not an HTTP connection");
+		try {
+			HttpURLConnection httpConn = (HttpURLConnection) conn;
+			httpConn.setAllowUserInteraction(false);
+			httpConn.setInstanceFollowRedirects(true);
+			httpConn.setRequestMethod("GET");
+			httpConn.connect();
+			response = httpConn.getResponseCode();
+			if (response == HttpURLConnection.HTTP_OK) {
+				in = httpConn.getInputStream();
+			}
+		} catch (Exception ex) {
+			throw new IOException("Error connecting");
+		}
+		return in;
+	}
+
+	private String DownloadText(String URL) {
+		int BUFFER_SIZE = 2000;
+		InputStream in = null;
+		try {
+			in = OpenHttpConnection(URL);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+			return "";
+		}
+		InputStreamReader isr = new InputStreamReader(in);
+		int charRead;
+		String str = "";
+		char[] inputBuffer = new char[BUFFER_SIZE];
+		try {
+			while ((charRead = isr.read(inputBuffer)) > 0) {
+				// convert the chars to a String
+				String readString = String
+						.copyValueOf(inputBuffer, 0, charRead);
+				str += readString;
+				inputBuffer = new char[BUFFER_SIZE];
+			}
+			in.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "";
+		}
+		return str;
+	}
+
+	// Check isInternetOn
+	public final boolean isInternetOn() {
+		ConnectivityManager connec = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		if (connec.getNetworkInfo(0).getState() == NetworkInfo.State.CONNECTED
+				|| connec.getNetworkInfo(0).getState() == NetworkInfo.State.CONNECTING
+				|| connec.getNetworkInfo(1).getState() == NetworkInfo.State.CONNECTING
+				|| connec.getNetworkInfo(1).getState() == NetworkInfo.State.CONNECTED) {
+			return true;
+		} else if (connec.getNetworkInfo(0).getState() == NetworkInfo.State.DISCONNECTED
+				|| connec.getNetworkInfo(1).getState() == NetworkInfo.State.DISCONNECTED) {
+			return false;
+		}
+		return false;
 	}
 
 }
